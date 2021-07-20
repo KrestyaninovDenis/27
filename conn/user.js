12cc49b9b2410240785b09e39ef2a4f82045f052
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-20 23:42:24 +0500

    Add files via upload

--- conn/user.js
+++ conn/user.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose');
 
 const UserDB = process.env.DB_USERNAME || 'root';
 const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
-const NameDB = 'Book'
+const NameDB = 'User' //
+// если имя разное - то две базы, если одинаковое - то две коллекции в одной базе
 const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
         
 const conn = mongoose.createConnection(HostDb, {
