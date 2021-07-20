a5972397a666eb3cb5180669de8db27f1ede0d2f
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-20 21:14:16 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,47 +19,8 @@ app.use('/user', userRouter);
 app.use(errorMiddleware);
 
 
-const PORT = process.env.PORT || 3000;
-const UserDB = process.env.DB_USERNAME || 'root';
-const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
-const NameDB = 'todos'
-const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
-
-async function start() {
-    try {
-        
-const conn = mongoose.createConnection(HostDb, {
-    user: UserDB,
-    pass: PasswordDB,
-    dbName: NameDB,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-
-const schema = new mongoose.Schema({ title: 'string', description: 'string', authors: 'string', favorite: 'string', fileCover: 'string', fileName: 'string', fileBook: 'string', date: 'string'});
-
-conn.model('Book', schema);
-
-//const conn002 = mongoose.createConnection(process.env.MONGODB_URI);
-//conn002.model('PageView', require('../schemas/pageView'));
-            
-/*          await mongoose.connect(HostDb, {
-            user: UserDB,
-            pass: PasswordDB,
-            dbName: NameDB,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-*/
-
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
-    } catch (e) {
-        console.log(e);
-    }
-}
-start();
 
 
