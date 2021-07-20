5e7711d4236e28178eda7d578a32ed3105a94b66
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-20 22:00:56 +0500

    Add files via upload

---
+++ conn/book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const {Schema, model} = require('mongoose');
+
+const UserDB = process.env.DB_USERNAME || 'root';
+const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
+const NameDB = 'Book'
+const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
+        
+const conn = mongoose.createConnection(HostDb, {
+    user: UserDB,
+    pass: PasswordDB,
+    dbName: NameDB,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const schema = new mongoose.Schema({ title: 'string', description: 'string', authors: 'string', favorite: 'string', fileCover: 'string', fileName: 'string', fileBook: 'string', date: 'string'});
+
+module.exports = conn.model('Book1', schema);
