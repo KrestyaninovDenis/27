a5b5ab7467c0a6880ea94533fe9cf37d2b4ad5b1
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-22 00:53:43 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -10,58 +10,6 @@ app.set("view engine", "ejs");
 
 //************************************************************ */
 
-var passport       = require('passport');
-var LocalStrategy  = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, function(username, password,done){
-  User.findOne({ user : username},function(err,user){
-    return err 
-      ? done(err)
-      : user
-        ? password === user.password
-          ? done(null, user)
-          : done(null, false, { message: 'Incorrect password.' })
-        : done(null, false, { message: 'Incorrect username.' });
-  });
-}));
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-  
-  
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err,user){
-      err 
-        ? done(err)
-        : done(null,user);
-    });
-  });
-  app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({ secret: 'SECRET' }));
- 
-// Passport:
-app.use(passport.initialize());
-app.use(passport.session());
-app.post('/user/login',                  controllers.users.login);
-module.exports.login = function(req, res, next) {
-    passport.authenticate('local',
-      function(err, user, info) {
-        return err 
-          ? next(err)
-          : user
-            ? req.logIn(user, function(err) {
-                return err
-                  ? next(err)
-                  : res.redirect('/private');
-              })
-            : res.redirect('/');
-      }
-    )(req, res, next);
-  };
 //************************************************************* */
 
 const errorMiddleware = require('./middleware/error');
