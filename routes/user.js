0126aef829c099b8fcdb07f230acc4474b5622ef
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-23 20:59:32 +0500

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+
+const User = require('./conn/user')
 /*
 GET /api/user/login   страница с формой входа / регистрации
 GET /api/user/me      страница профиля
